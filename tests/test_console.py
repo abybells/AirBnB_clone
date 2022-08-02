c1ecbf7a7643181950aa5169b7fc2f3da442ff48
abybells <abbiolbello@yahoo.com>
2022-08-02 12:04:07 -0700

    tests

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Unittest for the Console class
+Test files using the following command line:
+python3 -m unittest tests/test_console.py
+"""
+import unittest
+import pep8
+from os import path, remove
+
+
+class TestConsole(unittest.TEstCase):
+    """define variables and methods"""
+    pass
