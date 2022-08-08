9687d199050f449469845f204d935e0266a051b4
abybells <99332371+abybells@users.noreply.github.com>
2022-08-08 19:25:48 +0100

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,9 +8,9 @@ import unittest
 import pep8
 from os import path, remove
 import datetime
-import models
+#import models
 # from models import base_model
-from models import city
+from .models import city
 # from models.base_model import BaseModel
 from models.city import City
 # from models import engine
