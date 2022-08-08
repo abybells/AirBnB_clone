69d988f75e3076bba9dec9c1ef13ba3428ae3fa3
abybells <99332371+abybells@users.noreply.github.com>
2022-08-08 18:01:42 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -24,10 +24,7 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
-   # def __init__(self, *args, **kwargs):
-   #     '__init__ method for place'
-   #     if len(kwargs) > 0:
-   #         self.__dict__ = kwargs
-   #     else:
-   #         super().__init__(self)
+    
+    # def __init__(self, *args, **kwargs):
+    #     """initialize variables and methods"""
+    #     super().__init__(self, *args, **kwargs)
